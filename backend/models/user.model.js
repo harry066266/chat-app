c49a472398b614c5c9e89f49d21180a73a2274c6
harry066266 <162404750+harry066266@users.noreply.github.com>
2024-11-04 17:02:55 +1100

    backend part finished

---
+++ backend/models/user.model.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const userSchema = mongoose.Schema(
+  {
+    fullName: {
+      type: String,
+      require: true,
+    },
+    username: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      minlength: 6,
+    },
+    gender: {
+      type: String,
+      required: true,
+      enum: ["male", "female"],
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true }
+);
+
+const User = mongoose.model("User", userSchema);
+export default User;
